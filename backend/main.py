3de1997987f2747f543aae47746fcb78944c2712
Thanaphat Phosing <rgas.tr15@gmail.com>
2026-04-01 21:05:16 +0700

    feat: setup secrets handling and configure MySQL backend

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ import click
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+import os
 
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 from flask_jwt_extended import JWTManager
@@ -12,8 +13,8 @@ from models import TodoItem, Comment, db, User   # 👈 import จาก models.
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
-app.config['JWT_SECRET_KEY'] = 'fdsjkfjioi2rjshr2345hrsh043j5oij5545'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///todos.db')
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'fdslkfjsdlkufewhjroiewurewrew')
 jwt = JWTManager(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
